ed851bd47944b3bbd184b5dc3eda7c9942285c22
atish <atishlearn@gmail.com>
2025-07-29 11:46:53 +0530

    feat: initialize Python project with venv, requirements, and gitignore

---
+++ serve.py
@@ -0,0 +1,6 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("WeatherAssistant")
+
+# Print the MCP server instance to display its configuration and status
+print("MCP Server Instance:", mcp)
